394518034b7af3fa466f60c9951225d1deddd13d
Jitesh Ashok <jitesh.ashok@icloud.com>
2024-03-25 11:15:40 -0300

    comecei a segunda questao

--- EFC1/main.py
+++ EFC1/main.py
@@ -12,9 +12,12 @@ import matplotlib.pyplot as plt
 # Dom_flt (voos domesticos); Int_flt (voos internacionais); Flt (voos totais); Dom_RPM (Revenue Passenger-miles
 # (Domestic)); Int_RPM (Revenue Passenger-miles (International))
 
-df = pd.read_csv("D:/Usuario/Desktop/Backup/Jitesh/IA048/EFC1/air_traffic.csv")
+# df = pd.read_csv("D:/Usuario/Desktop/Backup/Jitesh/IA048/EFC1/air_traffic.csv")
+df = pd.read_csv("D:/Jitesh/Unicamp/IA048/EFC1/air_traffic.csv")
 
 df['date'] = pd.to_datetime(df[['Year', 'Month']].assign(DAY=1))
+
+
 
 total_flt = df['Flt']
 voos = []
@@ -26,7 +29,7 @@ t = df['date']
 
 df_new = pd.DataFrame({'X': voos, 'Y': t})
 
-print(df_new.head(10))
+# print(df_new.head(10))
 
 # Questao 1
 t1 = pd.Timestamp('2008-08-01')
@@ -35,11 +38,19 @@ t2 = pd.Timestamp('2019-12-01')
 plt.figure(figsize=(10, 6))
 plt.plot(t, voos, label='Numero total de voos')
 plt.axvline(t1, color='b', label='Intervalo 1')
-plt.axvline(t2, color='g', label='Intervalo 2')
-plt.xlabel('Time')
+plt.axvline(t2, color='r', label='Intervalo 2')
+plt.xlabel('Data')
 plt.ylabel('Numero de voos (em mil)')
 plt.legend()
 plt.grid(True)
 plt.show()
 
 # Questao 2
+
+index = pd.Index(df_new.Y).get_loc('2019-12-01')
+
+print(index)
+
+train = df_new.iloc[0:index+1].copy()
+
+# df_new['Y'] = (df_new.index.values + 0.5) / 12
