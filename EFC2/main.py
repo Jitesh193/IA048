cbfcf548368fd5d4c96ffe3db768d697efedd823
Jitesh Ashok <jitesh.ashok@icloud.com>
2024-04-12 09:56:58 -0300

    carreguei os dados da primeira parte

--- EFC2/main.py
+++ EFC2/main.py
@@ -4,3 +4,13 @@ Aluno: Jitesh Ashok Manilal Vassaram
 RA: 175867
 """
 
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+
+df = pd.read_csv("X_train.txt", sep='\s+', header=None)
+
+print(df)
+
+print(type(df[0][1]))
