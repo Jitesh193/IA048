0ac02921071fdca50e4fe31821f80b84f1a76afd
Jitesh Ashok <jitesh.ashok@icloud.com>
2024-04-12 10:59:50 -0300

    Inicio do treinamento da primeira parte

--- EFC2/main.py
+++ EFC2/main.py
@@ -4,11 +4,23 @@ Aluno: Jitesh Ashok Manilal Vassaram
 RA: 175867
 """
 
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 
 
-df = pd.read_csv("X_train.txt", sep='\s+', header=None)
+train_x = pd.read_csv("X_train.txt", sep='\s+', header=None)
 
-print(df)
+print(train_x.head(20))
+
+train_y = pd.read_csv("y_train.txt", sep='\s+', header=None)
+
+print(train_y.head(20))
+
+
+X = train_x[train_x.columns[0:]].values
+
+y = train_y.values
+
+softReg = LogisticRegression(multi_class='multinomial', solver='lbfgs')
