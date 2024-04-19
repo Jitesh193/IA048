1a60a1fc5708255a48b9af47278cd80a8a71d1d6
Jitesh Ashok <jitesh.ashok@icloud.com>
2024-04-19 14:58:43 -0300

    feito o item c da segunda parte! Terminado a tarefa!

--- EFC2/main_itemc_parte2.py
+++ EFC2/main_itemc_parte2.py
@@ -4,7 +4,8 @@ Aluno: Jitesh Ashok Manilal Vassaram
 RA: 175867
 """
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 from sklearn.metrics import confusion_matrix
@@ -82,36 +83,56 @@ GYR_zTe = GYR_zTe[GYR_zTe.columns[0:]].values
 x_test = np.concatenate((ACC_xTe, ACC_yTe, ACC_zTe, GYR_xTe, GYR_yTe, GYR_zTe), axis=1)
 y_test = pd.read_csv("y_test.txt", sep='\s+', header=None)
 
+# Separacao Conjunto de Treinamento e Validação
+X, X_val, y, y_val = train_test_split(x_train, y_train, test_size=0.3, random_state=42)
 
-# Treinamento:
-
-X = x_train.copy()
-
-y = y_train.values.copy()
 y = np.ravel(y)
+y_val = np.ravel(y_val)
 
-print(f'O tamanho da matriz X de treinamento: col:{np.size(X,1)}, linhas: {np.size(X,0)}')
 
-softReg = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=250)
-softReg.fit(X, y)
+# Validacao:
 
-# Teste:
-y_hat = softReg.predict(x_test)
+max_iter = 25
+Rec = np.zeros((max_iter, 6))
+bacc = []
 
-# Matriz de Confusao
-C = confusion_matrix(y_test, y_hat)
+for k in range(1, max_iter+1):
+
+    kNNReg = KNeighborsClassifier(n_neighbors=k, weights='distance')
+    kNNReg.fit(X, y)
+
+    y_hat = kNNReg.predict(X_val)
+
+    # Metricas de Avaliação Global
+    recall = recall_score(y_val, y_hat, average=None)
+    Rec[k-1, :] = recall
+    bA = np.sum(recall)/(len(recall))
+
+    bacc.append(bA)
+
+k_otimo = bacc.index(max(bacc))+1
+
+print(f'Os valores das Acurácia Balanceada são: \n {bacc} \n')
+print(f'O valor do k que gera o maior valor da Acurácia Balanceada é: {k_otimo}')
+print('--'*50)
+print(f'Os valores de recall para cada valor de k é: \n {Rec}')
+print('--'*50)
+
+# Re-treinamento com o k ótimo
+
+y2 = np.ravel(y_train)
+
+kNNReg = KNeighborsClassifier(n_neighbors=k_otimo)
+kNNReg.fit(x_train, y2)
+
+y_hat2 = kNNReg.predict(x_test)
+
+C = confusion_matrix(y_test, y_hat2)
 confusionMatrix = pd.DataFrame(data=C, index=['1, true', '2, true', '3, true', '4, true', '5, true', '6, true'], columns=['1, predicted', '2, predicted', '3, predicted', '4, predicted', '5, predicted', '6, predicted'])
 confusionMatrix.loc['sum'] = confusionMatrix.sum()
 confusionMatrix['sum'] = confusionMatrix.sum(axis=1)
 print(confusionMatrix)
 print('--'*50)
-
-# metrica global de avaliacao: Acuracia balanceada e recall
-print(f'A acuracia balanceada foi de: {balanced_accuracy_score(y_test,y_hat)}')
+print(f'A acuracia balanceada foi de: {balanced_accuracy_score(y_test,y_hat2)}')
 print('--'*50)
-
-recall = recall_score(y_test,y_hat,average=None)
-bacc = np.sum(recall)/(len(recall))
-print(f'O recall para o conjunto de teste é: \n {recall_score(y_test,y_hat,average=None)}')
-
-
+print(f'O recall para o conjunto de teste é: \n {recall_score(y_test,y_hat2,average=None)}')
